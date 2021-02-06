95bfa0a4f80c12bdfd2863902e9960bd50358eed
blakeifitz <blakeifitz@gmail.com>
2021-02-06 10:49:03 -0500

    changes

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
-
+const pg = require('pg');
+pg.defaults.ssl = true;
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
